be040afb688a688898e8e7243c5adf6f823f4ae8
Gab9431 <gabrielmajewska0@gmail.com>
2025-11-14 09:32:05 +0100

    Update script.js

--- TP-13/script.js
+++ TP-13/script.js
@@ -41,4 +41,42 @@ function cdc(){
     document.write("La chaine contient " + chaine.length + " caractéres" + "<br>");
     document.write("La première lettre est " + chaine.substr(0,1) + "<br>");
 
+
+}
+
+function seconnecter3(){
+    var i = 0;
+    do{
+        var id = prompt("Entrez votre nom d'utilisateur");
+        var mdp = prompt("Entrez votre mot de passe");
+        if (id == "admin" && mdp == "admin"){
+            document.write("bienvenue" + id);
+            break;
+        } else alert("Accès refusé");
+        i = i+1;
+    } while (i < 3);
+
+    if (i == 3) alert("Delai dépassé");
+}
+
+function ajouterligne(){
+    var name = document.getElementById("nom").value;
+    var prenom = document.getElementById("prenom").value;
+    var age = document.getElementById("age").value;
+
+    if(name == "" || age == "" || prenom == ""){
+        alert("un des deux éléments n'est pas complet ou valide");
+    } else if (age < 10 || age > 20){
+        alert("L'age n'est pas valide ");
+    }
+    else{
+        var table = document.getElementById("matable");
+        var newRow = table.insertRow(-1);
+        var cell1 = newRow.insertCell(0);
+        var cell2 = newRow.insertCell(1);
+        var cell3 = newRow.insertCell(2);
+        cell1.innerHTML = name;
+        cell2.innerHTML = prenom;
+        cell3.innerHTML = age;
+    }
 }
